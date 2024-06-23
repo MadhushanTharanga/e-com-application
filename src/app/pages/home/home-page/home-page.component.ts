a078a906811fbd5f6b2213bd269ac70e33691216
Madhushan Tharanga <110463634+MadhushanTharanga@users.noreply.github.com>
2024-06-23 13:38:00 +0530

    added some files and fixed router

--- src/app/pages/home/home-page/home-page.component.ts
+++ src/app/pages/home/home-page/home-page.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MainSliderComponent } from "./inner-items/main-slider/main-slider.component";
+import { ProductViewCardComponent } from "../../../share/cards/product-view-card/product-view-card.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-home-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+    selector: 'app-home-page',
+    standalone: true,
+    templateUrl: './home-page.component.html',
+    styleUrl: './home-page.component.scss',
+    imports: [MainSliderComponent, ProductViewCardComponent, RouterLink]
 })
 export class HomePageComponent {
 
--- src/app/pages/product/product-page/product-page.component.ts
+++ src/app/pages/product/product-page/product-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { ProductViewCardComponent } from "../../../share/cards/product-view-card/product-view-card.component";
 
 @Component({
-  selector: 'app-product-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './product-page.component.html',
-  styleUrl: './product-page.component.scss'
+    selector: 'app-product-page',
+    standalone: true,
+    templateUrl: './product-page.component.html',
+    styleUrl: './product-page.component.scss',
+    imports: [ProductViewCardComponent]
 })
 export class ProductPageComponent {
 
